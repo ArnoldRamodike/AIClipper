93ed809fffe8a95c70890d618380dccbbdd4aa17
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-30 14:39:33 +0200

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { Slot } from 'expo-router';
+
+const RootLAyout = () => {
+  return (
+   <Slot/>
+  )
+}
+
+export default RootLAyout
